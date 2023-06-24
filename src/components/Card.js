29aee4919b4df1c34366d300c6edf4ef770e3e9e
Devin Bielejec <devin.bielejec@gmail.com>
2023-06-24 11:03:45 -0400

    styled select menus custom drop down

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,22 @@
 import React from "react";
 import styled from "styled-components";
-import { Button } from "./Styles.js";
 import { updateDocumentQuestions } from "../actions/updateDocumentQuestions.js";
 import { connect } from "react-redux";
 import { baseURLStatic } from "../utils/index.js";
+import {
+  Flex,
+  Background,
+  Form,
+  SubmitButton,
+  StyledInput,
+  Warning,
+  StyledLink,
+  Text,
+  Button,
+} from "./Styles";
 
 const StyledCard = styled.div`
-  padding: 5px;
+  padding: 10px;
   width: 40%;
   border: 1px solid black;
   border-radius: 15px;
@@ -16,6 +26,7 @@ const StyledCard = styled.div`
   justify-content: space-between;
   align-items: center;
   max-width: 400px;
+  background-color: white;
 `;
 
 const Image = styled.img`
@@ -60,9 +71,10 @@ function Card({
           {kwarg}:{question.kwargs[kwarg]}
         </p>
       ))}
-      <input
+      <StyledInput
         type="number"
         name="questionCount"
+        placehold={questionCount}
         value={questionCount}
         onChange={handleQuestionCountChange}
       />
