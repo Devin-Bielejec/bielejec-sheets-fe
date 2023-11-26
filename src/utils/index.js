63e0bcfc7ff760cc441c9ded0626f023de1039cc
Devin Bielejec <devin.bielejec@gmail.com>
2023-11-26 18:15:27 -0500

    removed slashes after base urls

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 export const baseURL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:5000/"
-    : "https://bielejec-sheets-be.vercel.app/";
+    ? "http://localhost:5000"
+    : "https://bielejec-sheets-be.vercel.app";
 
 export const baseURLStatic =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:5000/"
-    : "https://bielejec-sheets-be.vercel.app/";
+    ? "http://localhost:5000"
+    : "https://bielejec-sheets-be.vercel.app";
 
 //Makes axios call and send token if existing
 export const axiosWithAuth = () => {
